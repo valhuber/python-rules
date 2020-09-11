733d0f19528178c94b98f599713d16e5a01bfb50
valhuber <valjhuber@gmail.com>
2020-09-11 08:57:44 -0700

    clean up comments

--- nw/nw_logic/nw_rules_bank.py
+++ nw/nw_logic/nw_rules_bank.py
@@ -8,8 +8,35 @@ def activate_basic_check_credit_rules():
     Issues function calls to activate check credit rules, below.
         These rules are executed not now, but on commits
         Their inclusion in classes is for doc / convenience, no semantics
-    Rules (below) can invoke Python functions
+
+    These rules apply to all transactions (automatic re-use), eg.
+    * place order
+    * change Order Detail product, amount
+    * add/delete Order Detail
+    * ship / unship order
+    * delete order
+    * move order to new customer
+    * etc
     """
+
+    def my_early_event(row, old_row, logic_row):
+        logic_row.log("early event for *all* tables - good breakpoint, time/date stamping, etc")
+
+    Logic.constraint_rule(validate="Customer", as_condition="row.Balance <= row.CreditLimit",
+                          error_msg="balance ({row.Balance}) exceeds credit ({row.CreditLimit})")
+    Logic.sum_rule(derive="Customer.Balance", as_sum_of="OrderList.AmountTotal",
+                   where="row.ShippedDate is None")
+
+    Logic.sum_rule(derive="Order.AmountTotal", as_sum_of="OrderDetailList.Amount")
+
+    Logic.formula_rule(derive="OrderDetail.Amount",  # or, calling=compute_amount)
+                       as_exp="row.UnitPrice * row.Quantity")
+    Logic.copy_rule(derive="OrderDetail.UnitPrice", from_parent="ProductOrdered.UnitPrice")
+
+    Logic.early_row_event_rule(on_class="*", calling=my_early_event)  # just for debug
+
+
+class InvokePythonFunctions:
 
     def check_balance(row, old_row, logic_row) -> bool:
         """
@@ -24,32 +51,10 @@ def activate_basic_check_credit_rules():
         """
         return row.UnitPrice * row.Quantity
 
-    def my_early_event(row, old_row, logic_row):
-        logic_row.log("early event for *all* tables - good breakpoint, time/date stamping, etc")
-
-    """
-    These rules apply to all transactions, automatically, eg.
-    * place order
-    * change Order Detail product, amount
-    * add/delete Order Detail
-    * ship / unship order
-    * delete order
-    * move order to new customer
-    * etc
-    """
-
-    Logic.constraint_rule(validate="Customer", as_condition="row.Balance <= row.CreditLimit",
+    Logic.constraint_rule(validate="Customer", calling=check_balance,
                           error_msg="balance ({row.Balance}) exceeds credit ({row.CreditLimit})")
-    Logic.sum_rule(derive="Customer.Balance", as_sum_of="OrderList.AmountTotal",
-                   where="row.ShippedDate is None")
 
-    Logic.sum_rule(derive="Order.AmountTotal", as_sum_of="OrderDetailList.Amount")
-
-    Logic.formula_rule(derive="OrderDetail.Amount",  # or, calling=compute_amount)
-                       as_exp="row.UnitPrice * row.Quantity")
-    Logic.copy_rule(derive="OrderDetail.UnitPrice", from_parent="ProductOrdered.UnitPrice")
-
-    Logic.early_row_event_rule(on_class="*", calling=my_early_event)
+    Logic.formula_rule(derive="OrderDetail.Amount", calling=compute_amount)
 
 
 class DependencyGraphTests:
